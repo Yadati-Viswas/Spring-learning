74aa7d477374f23095391be31a3bc51623b7abc0
Yadati <yadatisaiviswas3@gmail.com>
2024-01-19 20:13:28 +0530

    Spring Boot: Spring Basics, JPA, Hibernate, MySQL

--- src/main/java/com/sample/spring/Service/ServiceClass.java
+++
@@ -1,9 +0,0 @@
-package com.sample.spring.Service;
-
-import org.springframework.stereotype.Service;
-@Service
-public class ServiceClass {
-    public String getMessage(){
-        return "Hello World";
-    }
-}
